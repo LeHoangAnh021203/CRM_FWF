5eef70489c2190c6a36bd79e6ef4b5cbca13477e
LE HOANG ANH <lehoanganh@MacBook-Pro-cua-LE.local>
2025-07-16 18:22:37 +0700

    Final code

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -20,9 +20,9 @@ export function Header() {
           <Button variant="ghost" size="sm" className="lg:hidden">
             <Menu className="h-5 w-5" />
           </Button>
-          <div className="relative">
+          <div className="relative ">
             <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400 h-4 w-4" />
-            <Input placeholder="Search..." className="pl-10 w-64" />
+            <Input placeholder="Search..." className="pl-10 w-64 " />
           </div>
         </div>
 
